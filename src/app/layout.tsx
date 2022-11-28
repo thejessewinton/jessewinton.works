03cd9afe9296ff265dadffdc28e51d21268a60ad
jwinton@veroskills.com <jwinton@veroskills.com>
2022-11-28 10:40:25 -0500

    Update to next13

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+
+import { Inter } from '@next/font/google';
+import '../styles/globals.css';
+import classNames from 'classnames';
+
+const inter = Inter();
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  const title = 'Jesse Winton - Full Stack Software Engineer';
+  const description =
+    'Full-stack web developer with a passion for building beautiful, performant, and accessible websites. Head of engineering at VeroSkills.com';
+  return (
+    <html
+      className={classNames(
+        inter.className,
+        'min-h-screen bg-neutral-900 font-sans text-base lg:text-lg selection:bg-gray-400 selection:text-gray-100 w-screen overflow-x-hidden overflow-y-scroll box-border antialiased'
+      )}
+    >
+      <head>
+        <title>{title}</title>
+        <link rel="shortcut icon" href="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={description}></meta>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content={title} />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+      </head>
+
+      <body className="relative m-0 w-screen text-white leading-6">
+        <main>
+          <div className="min-h-screen flex flex-col items-center justify-center">
+            {children}
+          </div>
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { HTMLAttributeAnchorTarget } from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
-import { Head } from 'components/Head';
-
 const Button = ({
   href,
   target = '_self',
@@ -15,14 +13,13 @@ const Button = ({
   children: React.ReactNode;
 }) => {
   return (
-    <Link href={href}>
-      <a
-        target={target}
-        rel="noopener noreferrer"
-        className="py-3 px-6 max-h-12 font-semibold cursor-pointer flex items-center justify-center rounded transition-all bg-gray-400 text-gray-100 gap-3 text-xs lg:text-lg hover:bg-transparent hover:border-gray-400 border border-solid border-transparent relative overflow-hidden hover:bg-gray-100 hover:text-white disabled:cursor-not-allowed"
-      >
-        {children}
-      </a>
+    <Link
+      href={href}
+      target={target}
+      rel="noopener noreferrer"
+      className="py-3 px-6 max-h-12 font-semibold cursor-pointer flex items-center justify-center rounded transition-all bg-gray-100 text-neutral-900 gap-3 text-xs lg:text-lg hover:bg-transparent hover:text-white hover:border-white border border-solid border-transparent relative overflow-hidden  disabled:cursor-not-allowed"
+    >
+      {children}
     </Link>
   );
 };
@@ -30,10 +27,9 @@ const Button = ({
 const Index: NextPage = () => {
   return (
     <>
-      <Head title="Full-stack engineer in New York, NY" />
       <div className="container">
         <div className="flex flex-col gap-6 max-w-2xl mx-auto">
-          <h1 className="leading-tight mb-6">Next.js, baby!</h1>
+          <h1 className="leading-tight mb-6 text-6xl">Next.js, baby!</h1>
 
           <p>
             I am a NYC based full-stack engineer specializing in creating
