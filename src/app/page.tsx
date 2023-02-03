e90d5084973ed4852b9447935027349ce9bf5ef1
jwinton@veroskills.com <jwinton@veroskills.com>
2023-02-03 12:47:55 -0500

    Add copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export const revalidate = 3600;
 
 const Index = async () => {
@@ -14,8 +16,20 @@ const Index = async () => {
         </p>
         <br />
         <p>
-          Currently on the hunt to join a team of passionate and talented
+          Currently on the hunt to join a new team of passionate and talented
           engineers to build beautiful and performant web applications.
+        </p>
+        <br />
+        <p>
+          Formerly head of engineering at{' '}
+          <Link
+            href="https://veroskills.com"
+            target="_blank"
+            className="underline-offset-4 underline italic font-serif"
+          >
+            VeroSkills
+          </Link>
+          .
         </p>
       </div>
     </div>
