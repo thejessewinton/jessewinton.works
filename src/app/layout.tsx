0c322f3966922f808e60a766b2475735fa5d1362
jwinton@veroskills.com <jwinton@veroskills.com>
2023-02-04 16:55:12 -0500

    Fix mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
     <html lang="en" className={`${inter.variable} ${newsreader.variable}`}>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="/favicon.ico" rel="shortcut icon" />
-      <body className="flex min-h-screen flex-col scroll-smooth leading-loose antialiased selection:bg-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
-        <main className="mx-auto flex w-full max-w-3xl flex-grow flex-col px-8 pt-24 lg:pt-48">
+      <body className="flex min-h-screen flex-col justify-center items-center scroll-smooth leading-loose antialiased selection:bg-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
+        <main className="mx-auto flex w-full max-w-3xl flex-grow justify-center items-center flex-col px-8">
           {children}
         </main>
         <Footer />
