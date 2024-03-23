21df5b7f6da09d84510f4c4b3c60c14a9edac7b6
Jesse Winton <jrandallwinton@gmail.com>
2024-03-23 12:26:04 -0400

    fix

--- src/content.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs'
-import { groupBy, sortBy } from 'remeda'
-import { allExperiences } from '~/contentlayer'
-
-export const getAllExperience = () => {
-  const all = groupBy(allExperiences, (x) => dayjs(x.startDate).year())
-  return sortBy(Object.entries(all), ([year]) => -year)
-}
