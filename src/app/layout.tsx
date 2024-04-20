a3343140b98f17f5ae312a27f74df0f279629752
Jesse Winton <jrandallwinton@gmail.com>
2024-04-19 23:10:32 -0400

    tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <link href='/favicon.ico' rel='shortcut icon' />
       <body className='flex min-h-screen flex-col items-center justify-center scroll-smooth leading-loose antialiased selection:bg-neutral-100 dark:bg-neutral-900 dark:text-neutral-200 dark:selection:bg-neutral-800'>
-        <main className='mx-auto mt-8 flex w-full max-w-3xl flex-grow flex-col items-center justify-center px-8'>
+        <main className='mx-auto mt-32 flex w-full max-w-3xl flex-grow flex-col items-center justify-center px-8'>
           {children}
         </main>
         <Footer />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function Index() {
   return (
-    <div className='mt-40 flex flex-col gap-2 pb-4'>
+    <div className='flex flex-col gap-2 pb-4'>
       <div className='relative z-50 animate-enter'>
         <h1 className='group relative mb-4 inline-block w-full max-w-xs cursor-pointer font-medium'>{index.title}</h1>
 
@@ -35,38 +35,6 @@ export default function Index() {
               </div>
             </Link>
           ))}
-        </div>
-      ) : null}
-
-      {index.experiences ? (
-        <div className='group relative my-8 flex w-full animate-enter flex-col divide-y divide-neutral-300 overflow-hidden border-t border-neutral-300 animation-delay-500 dark:divide-neutral-800 dark:border-neutral-800'>
-          {index.experiences.map((experience) => {
-            return (
-              <Link
-                href={experience.url || ''}
-                target={experience.url?.startsWith('http') ? '_blank' : '_self'}
-                className='relative top-0 flex items-start gap-20'
-                key={experience.company}
-              >
-                <time className='absolute -z-20 pt-6 text-sm text-neutral-500'>{experience.endDate}</time>
-
-                <div className='ml-[25%] flex-1 divide-y divide-neutral-300 dark:divide-neutral-800'>
-                  <div className='flex-1 items-center justify-between py-6 transition-all before:absolute before:-left-1/2 before:h-full before:w-full before:content-[""] md:flex hover:md:!opacity-100 hover:md:!blur-none group-hover:md:opacity-40 group-hover:md:blur-xs'>
-                    <div>
-                      <span className='block text-xs text-neutral-500'>{experience.company}</span>
-                      {experience.role}
-                      <div
-                        className='prose prose-neutral mt-2 max-w-none text-neutral-500 dark:prose-invert prose-a:font-normal prose-a:no-underline prose-em:italic prose-code:rounded prose-code:bg-neutral-100 prose-code:px-1 prose-code:py-0.5 prose-code:font-mono prose-code:font-extralight prose-code:text-neutral-800 prose-code:before:content-none prose-code:after:content-none prose-code:dark:bg-neutral-950 prose-code:dark:text-white'
-                        dangerouslySetInnerHTML={{
-                          __html: experience.description.html
-                        }}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            )
-          })}
         </div>
       ) : null}
     </div>
