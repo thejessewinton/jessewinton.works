d26482bb6a39deb97761ae3d53bd09db1c1fcead
Jesse Winton <jesse@appwrite.io>
2024-11-27 18:59:36 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,79 +23,138 @@ export default function Index() {
         />
       </div>
 
-      <div className="group pointer-events-none relative my-12">
-        <h3
-          className="mb-8 animate-enter font-medium"
-          style={{
-            animationDelay: `${300}ms`,
-          }}
-        >
-          Work
-        </h3>
-        <div className="flex flex-col gap-12">
-          {index.works.map((work, i) => (
-            <Link
-              href={work.url}
-              style={{
-                animationDelay: `${i * 150 + 300}ms`,
-              }}
-              key={work.dates}
-              className="pointer-events-auto block animate-enter"
-            >
-              <div className="relative block space-y-5 font-light transition-all md:group-hover:opacity-40 md:group-hover:blur-xs md:hover:opacity-100! md:hover:blur-none!">
-                <header className="flex items-center justify-between gap-5">
-                  <div className="flex items-center font-normal">
-                    <span>{work.company}</span>
-                    <span>, {work.title}</span>
-                  </div>
-                  <div className="h-px grow bg-neutral-300 dark:bg-neutral-800" />
-                  <span className="text-neutral-900 dark:text-neutral-400">
-                    {work.dates}
-                  </span>
-                </header>
-                <div className="text-neutral-900 dark:text-neutral-400">
-                  {work.description}
+      <div className="mt-12 space-y-12">
+        <div className="group pointer-events-none relative">
+          <h3
+            className="mb-8 animate-enter font-medium"
+            style={{
+              animationDelay: `${300}ms`,
+            }}
+          >
+            Tech
+          </h3>
+          <div className="flex flex-wrap gap-3">
+            {index.tech.map((item, i) => {
+              return (
+                <div
+                  style={{
+                    animationDelay: `${i * 150 + 300}ms`,
+                  }}
+                  key={item}
+                  className="pointer-events-auto block animate-enter rounded-full border border-neutral-700/20 bg-neutral-800 px-3"
+                >
+                  {item}
                 </div>
-              </div>
-            </Link>
-          ))}
+              )
+            })}
+          </div>
         </div>
-      </div>
-      <div className="group pointer-events-none relative mt-4 mb-12">
-        <h3
-          className="mb-8 animate-enter font-medium"
-          style={{
-            animationDelay: `${300}ms`,
-          }}
-        >
-          Projects
-        </h3>
-        <div className="flex flex-col gap-12">
-          {index.projects.map((project, i) => (
-            <Link
-              href={project.url}
-              style={{
-                animationDelay: `${i * 150 + 300}ms`,
-              }}
-              key={project.title}
-              className="pointer-events-auto block animate-enter"
-            >
-              <div className="relative block space-y-5 font-light transition-all md:group-hover:opacity-40 md:group-hover:blur-xs md:hover:opacity-100! md:hover:blur-none!">
-                <header className="flex items-center justify-between gap-5">
-                  <div className="flex items-center font-normal">
-                    <span>{project.title}</span>
+
+        <div className="group pointer-events-none relative">
+          <h3
+            className="mb-8 animate-enter font-medium"
+            style={{
+              animationDelay: `${300}ms`,
+            }}
+          >
+            Work
+          </h3>
+          <div className="flex flex-col gap-12">
+            {index.works.map((work, i) => {
+              return (
+                <Link
+                  href={work.url}
+                  style={{
+                    animationDelay: `${i * 150 + 300}ms`,
+                  }}
+                  key={work.dates}
+                  className="pointer-events-auto block animate-enter"
+                >
+                  <div className="relative block space-y-5 font-light transition-all md:group-hover:opacity-40 md:group-hover:blur-xs md:hover:opacity-100! md:hover:blur-none!">
+                    <header className="flex items-center justify-between gap-5">
+                      <div className="flex items-center font-normal">
+                        <span>{work.company}</span>
+                        <span>, {work.title}</span>
+                      </div>
+                      <div className="h-px grow bg-neutral-300 dark:bg-neutral-800" />
+                      <span className="text-neutral-900 dark:text-neutral-400">
+                        {work.dates}
+                      </span>
+                    </header>
+                    <div className="text-neutral-900 dark:text-neutral-400">
+                      {work.description}
+                    </div>
                   </div>
-                  <div className="h-px grow bg-neutral-300 dark:bg-neutral-800" />
-                  <span className="text-neutral-900 dark:text-neutral-400">
-                    {project.url.replace(/https?:\/\//, '')}
-                  </span>
-                </header>
-                <div className="text-neutral-900 dark:text-neutral-400">
-                  {project.description}
-                </div>
-              </div>
-            </Link>
-          ))}
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+        <div className="group pointer-events-none relative">
+          <h3
+            className="mb-8 animate-enter font-medium"
+            style={{
+              animationDelay: `${300}ms`,
+            }}
+          >
+            Projects
+          </h3>
+          <div className="flex flex-col gap-12">
+            {index.projects.map((project, i) => {
+              return (
+                <>
+                  {project.url ? (
+                    <Link
+                      href={project.url}
+                      style={{
+                        animationDelay: `${i * 150 + 300}ms`,
+                      }}
+                      key={project.title}
+                      className="pointer-events-auto block animate-enter"
+                    >
+                      <div className="relative block space-y-5 font-light transition-all md:group-hover:opacity-40 md:group-hover:blur-xs md:hover:opacity-100! md:hover:blur-none!">
+                        <header className="flex items-center justify-between gap-5">
+                          <div className="flex items-center font-normal">
+                            <span>{project.title}</span>
+                          </div>
+                          <div className="h-px grow bg-neutral-300 dark:bg-neutral-800" />
+                          <span className="text-neutral-900 dark:text-neutral-400">
+                            {project.url.replace(/https?:\/\//, '')}
+                          </span>
+                        </header>
+                        <div className="text-neutral-900 dark:text-neutral-400">
+                          {project.description}
+                        </div>
+                      </div>
+                    </Link>
+                  ) : (
+                    <div
+                      style={{
+                        animationDelay: `${i * 150 + 300}ms`,
+                      }}
+                      key={project.title}
+                      className="pointer-events-auto block animate-enter"
+                    >
+                      <div className="relative block space-y-5 font-light transition-all md:group-hover:opacity-40 md:group-hover:blur-xs md:hover:opacity-100! md:hover:blur-none!">
+                        <header className="flex items-center justify-between gap-5">
+                          <div className="flex items-center font-normal">
+                            <span>{project.title}</span>
+                          </div>
+                          <div className="h-px grow bg-neutral-300 dark:bg-neutral-800" />
+                          <span className="text-neutral-900 dark:text-neutral-400">
+                            Coming soon
+                          </span>
+                        </header>
+                        <div className="text-neutral-900 dark:text-neutral-400">
+                          {project.description}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/content/index.md
+++ src/content/index.md
@@ -1,36 +1,50 @@
 ---
-title: 'Jesse Winton'
-description: 'NYC based design engineer, specializing in creating exceptional web experiences with modern frameworks.'
+title: Jesse Winton
+description: NYC based design engineer, specializing in creating exceptional web experiences with modern frameworks.
+tech: 
+  - React
+  - Typescript
+  - Next.js
+  - Tailwind
+  - Framer Motion
+  - GraphQL
+  - Node.js
+  - MySQL
+  - Postgres
+  - SvelteKit
 works:
-  - dates: '2023-24'
-    company: 'PlanetScale'
-    title: 'Design Engineer'
-    description: 'Design engineering for an elite MySQL database.'
-    url: 'https://planetscale.com'
-  - dates: '2021-23'
-    company: 'VeroSkills'
-    title: 'Head of Engineering'
-    description: 'A simple, delightful notes app. Experimenting with new design patterns.'
-    url: 'https://veroskills.com'
-  - dates: '2020-21'
-    company: 'One Day Doors & Closets'
-    title: 'Frontend Lead'
-    description: 'A simple, delightful notes app. Experimenting with new design patterns.'
-    url: 'https://onedaydoorsandclosets.com'
-  - dates: '2010-20'
-    company: 'Freelance'
-    title: 'Designer & Engineer'
-    description: 'A simple, delightful notes app. Experimenting with new design patterns.'
-    url: '/'
+  - dates: 2023-24
+    company: PlanetScale
+    title: Senior Design Engineer
+    description: Design engineering for an elite MySQL database. Worked on all features and surfaces of the marketing website, including global search, product documentation, growth features, and PlanetScales Resources Hub.
+    url: https://planetscale.com
+  - dates: 2021-23
+    company: VeroSkills
+    title: Co-Founder & Head of Engineering
+    description: Rebuilt the alpha version of the platform and launched to general availability. Led a team of 12+ remote engineers and managed development of the product, including database and API design, product roadmapping, and front end engineering.
+    url: https://veroskills.com
+  - dates: 2020-21
+    company: One Day Doors & Closets
+    title: Front End Lead
+    description: Led a full rebuild of the marketing site, building a performant, dynamic Next.js application, allowing for efficient management of dealerships and location specific content to an average of 100k unique monthly visitors. With an external consultant, responsible for the design and implementation of a full company rebrand.
+    url: https://onedaydoorsandclosets.com
+  - dates: 2010-20
+    company: Freelance
+    title: Designer & Engineer
+    description: Worked with agencies, individuals, and companies to ensure clear goals, and make technological decisions to ensure performant, well-designed, and well developed experiences across the web.
+    url: /
 projects:
-  - title: 'rpshr'
-    description: 'A simple, delightful notes app. Experimenting with new design patterns.'
-    url: 'https://rpshr.app'
-  - title: 'tinylight'
-    description: 'A beautifully designed, simple, native video component for React.'
-    url: 'https://tinylight.jessewinton.dev'
+  - title: rpshr
+    description: A simple, delightful notes app. Experimenting with new design patterns.
+    url: https://rpshr.app
+  - title: tinylight
+    description: A beautifully designed, open source video component for React.
+  - title: lab
+    description: A beautifully designed, simple, native video component for React.
+    url: https://lab.jessewinton.dev
 ---
 
 New York City based software engineer, specializing in building exceptional, performant products, delightful interactions and user experiences, and beautifully designed surfaces on the web. Typescript and type-safety die hard, and a huge fan of React. Writer, and have also written and performed original work off off Broadway. Management experience, but currently preferring to write code.
 
-Design Engineer at [Appwrite](https://appwrite.io).
+Design Engineer at [Appwrite](https://appwrite.io). View my [resume](/docs/resume.pdf) or [contact me](mailto:jrandallwinton@gmail.com) for more information.
+
