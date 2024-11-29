908753e347144b0625dcbb90aa51ad5e065f6fd4
Jesse Winton <jesse@appwrite.io>
2024-11-29 09:40:57 -0500

    sort classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Index() {
                     animationDelay: `${i * 150 + 300}ms`,
                   }}
                   key={item}
-                  className="block animate-enter rounded-full border border-neutral-300/20 dark:border-neutral-700/20 bg-neutral-100 dark:bg-neutral-800 px-3"
+                  className="block animate-enter rounded-full border border-neutral-300/20 bg-neutral-100 px-3 dark:border-neutral-700/20 dark:bg-neutral-800"
                 >
                   {item}
                 </div>
