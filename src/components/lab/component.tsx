6b1d74906d07e5f8bb764b00281aea76b86b33ae
Jesse Winton <jesse@appwrite.io>
2024-12-25 16:43:39 -0800

    update

--- src/components/lab/component.tsx
+++ src/components/lab/component.tsx
@@ -7,13 +7,13 @@ export const Component = ({
   tools,
   children,
   beta = false,
-  source,
+  filename,
 }: {
   title: string
   description: ReactNode
   tools: Array<string>
   children: ReactNode
-  source?: string
+  filename?: string
   beta?: boolean
 }) => {
   if (beta && process.env.NODE_ENV === 'production') return null
@@ -22,7 +22,15 @@ export const Component = ({
     <div className="flex min-h-[60vh] flex-col justify-center gap-8">
       <div className="order-2 flex flex-col gap-2 font-light md:order-1 md:col-span-4 md:mt-10">
         <div className="flex items-center gap-4">
-          <h2 className="font-medium">{title}</h2>
+          <Link
+            href={`https://github.com/thejessewinton/jessewinton.works/blob/main/src/components/lab/${filename}`}
+            className="font-medium"
+            title="View source"
+            target="_blank"
+          >
+            {title}
+          </Link>
+
           {beta ? (
             <span className="rounded-sm bg-blue-400 px-2 py-px text-black">
               Beta
@@ -30,7 +38,7 @@ export const Component = ({
           ) : null}
         </div>
         <p>{description}</p>
-        <ul className="mt-4 flex gap-2 text-sm">
+        <ul className="mt-4 flex items-center gap-2 text-sm">
           {tools.map((tool) => (
             <li
               key={tool}
@@ -44,8 +52,6 @@ export const Component = ({
       <div className="relative order-1 flex flex-1 items-center justify-center overflow-hidden rounded-2xl border border-neutral-300 p-10 md:order-2 md:col-span-8 dark:border-neutral-700/30">
         {children}
       </div>
-
-      {source && <Link href={source}>Source code</Link>}
     </div>
   )
 }
