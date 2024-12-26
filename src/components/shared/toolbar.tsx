b6e4a802767833037cf4118b665108250f64e594
Jesse Winton <jesse@appwrite.io>
2024-12-25 16:00:11 -0800

    update

--- src/components/shared/toolbar.tsx
+++ src/components/shared/toolbar.tsx
@@ -1,16 +1,17 @@
-'use client';
+'use client'
 
-import { useTheme } from 'next-themes';
+import { useTheme } from 'next-themes'
 
 export const Toolbar = () => {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme()
 
   return (
-    <div className="flex items-center justify-between p-4 max-w-xs left-1/2 w-full border border-neutral-300 dark:border-neutral-700/30 rounded-full -translate-x-1/2 bg-white/50 backdrop-blur-sm dark:bg-neutral-900/50 fixed mx-auto bottom-10">
+    <div className="-translate-x-1/2 fixed bottom-10 left-1/2 mx-auto flex w-full max-w-xs items-center justify-between rounded-full border border-neutral-300 bg-white/50 p-4 backdrop-blur-sm dark:border-neutral-700/30 dark:bg-neutral-900/50">
       <button
+        type="button"
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-        className="bg-neutral-300 dark:bg-neutral-700 size-6 rounded-full"
-      ></button>
+        className="size-6 rounded-full bg-neutral-300 dark:bg-neutral-700"
+      />
     </div>
-  );
-};
+  )
+}
