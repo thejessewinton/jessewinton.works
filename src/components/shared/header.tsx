7ce63f4543aae12e6399c67d20896f5239fbb4f1
Jesse Winton <jesse@appwrite.io>
2024-12-25 16:11:58 -0800

    update

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,24 +1,21 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 export const Header = () => {
   return (
-    <header className="flex justify-between container py-10 px-8 mx-auto">
-      <h1 className="text-sm">
+    <header className="container mx-auto mb-10 flex justify-between">
+      <h1 className="font-medium">
         Jesse Winton /{' '}
-        <span className="font-serif italic font-light">The Lab</span>
+        <span className="font-normal font-serif italic">The Lab</span>
       </h1>
-      <Link
-        href="https://jessewinton.works"
-        className="group items-center flex gap-2 justify-center"
-      >
-        <span className="font-serif italic text-sm font-light">Home</span>
+      <Link href="/" className="group flex items-center justify-center gap-2">
+        <span className="font-normal font-serif italic">Home</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth="1.5"
           stroke="currentColor"
-          className="size-3 group-hover:-rotate-12 transition-all"
+          className="group-hover:-rotate-12 size-3 transition-all"
         >
           <path
             strokeLinecap="round"
@@ -28,5 +25,5 @@ export const Header = () => {
         </svg>
       </Link>
     </header>
-  );
-};
+  )
+}
