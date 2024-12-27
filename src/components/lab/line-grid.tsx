e76e3854174ef9d5d6417c76f834f3d0720702b9
Jesse Winton <jesse@appwrite.io>
2024-12-27 00:19:26 -0800

    tweak

--- src/components/lab/line-grid.tsx
+++ src/components/lab/line-grid.tsx
@@ -1,5 +1,26 @@
 'use client'
 
+import { motion } from 'motion/react'
+import { useMousePosition } from '~/hooks/use-mouse-position'
+
 export const LineGrid = () => {
-  return <div>Map</div>
+  const items = Array.from({ length: 40 }, (_, i) => i)
+  const { x, y } = useMousePosition()
+
+  return (
+    <div className="flex items-center gap-3">
+      {items.map((_, i) => {
+        return (
+          <motion.div
+            className="h-10 w-px bg-neutral-300 dark:bg-neutral-700"
+            key={i}
+            animate={{
+              scale: x && y ? 1.5 : 1,
+              rotate: x && y ? 360 : 0,
+            }}
+          />
+        )
+      })}
+    </div>
+  )
 }
