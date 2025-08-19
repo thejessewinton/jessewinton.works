e8d608ea1e52102cd8831da18517d408898d6839
Jesse Winton <jesse@appwrite.io>
2025-08-19 19:02:10 -0400

    tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { ArrowUpRight } from '@phosphor-icons/react/dist/ssr'
 import type { Metadata } from 'next'
+import Link from 'next/link'
 import { Clock } from '~/components/shared/clock'
+import { Glitch } from '~/components/shared/glitch'
 import { Weather } from '~/components/shared/weather'
 
 import { allIndices } from '~/content-collections'
@@ -14,10 +16,15 @@ export const metadata: Metadata = {
 
 export default function Index() {
   return (
-    <div className="flex w-full flex-col flex-wrap justify-between gap-20 leading-tight md:flex-row">
+    <div className="grid w-full grid-cols-1 justify-between gap-12 leading-tight md:grid-cols-[repeat(2,max-content)] md:gap-30 lg:grid-cols-[repeat(4,max-content)]">
       <div className="flex flex-col gap-8">
         <span className="font-medium ">Jesse Winton</span>
-        <p>{index.now}</p>
+        <span>
+          {index.now.title} —{' '}
+          <Link href={index.now.url} target="_blank">
+            <Glitch text={index.now.company} />
+          </Link>
+        </span>
       </div>
       <div className="flex flex-col gap-8">
         <span className="font-medium">NYC</span>
@@ -40,25 +47,48 @@ export default function Index() {
           {index.experience.map((experience) => {
             return (
               <p key={experience.company}>
-                {experience.dates} — {experience.title}
-                <span className="font-medium"> [{experience.company}]</span>
+                <span className="tabular-nums tracking-tighter">
+                  {experience.dates}
+                </span>{' '}
+                — {experience.title}
+                <span className="font-medium"> {experience.company}</span>
               </p>
             )
           })}
         </div>
       </div>
       <div className="flex flex-col gap-8">
+        <span className="font-medium">Projects</span>
+        <div className="flex flex-col">
+          {index.projects.map((project) => {
+            return (
+              <Link
+                href={project.url}
+                key={project.title}
+                className="group flex items-center justify-between gap-1 py-1 font-medium first-of-type:pt-0"
+              >
+                <span>{project.title}</span>
+                <ArrowUpRight className="opacity-0 blur-sm transition-all group-hover:opacity-100 group-hover:blur-none" />
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+      <div className="flex flex-col gap-8">
         <span className="font-medium">Connect</span>
-        <div className="flex flex-col gap-2 leading-tight">
-          <a href="/" className="flex items-center justify-between gap-1">
-            [Github] <ArrowUpRight />
-          </a>
-          <a href="/" className="flex items-center justify-between gap-1">
-            [Twitter] <ArrowUpRight />
-          </a>
-          <a href="/" className="flex items-center justify-between gap-1">
-            [Email] <ArrowUpRight />
-          </a>
+        <div className="flex flex-col">
+          {index.connections.map((connection) => {
+            return (
+              <Link
+                href={connection.url}
+                key={connection.title}
+                className="group flex items-center justify-between py-1 font-medium first-of-type:pt-0"
+              >
+                <span>{connection.title}</span>
+                <ArrowUpRight className="opacity-0 blur-sm transition-all group-hover:opacity-100 group-hover:blur-none" />
+              </Link>
+            )
+          })}
         </div>
       </div>
     </div>
